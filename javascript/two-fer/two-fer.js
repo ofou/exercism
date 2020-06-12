672d7348a53184d7e14c7eb7685adc2014d70b5b
Omar Olivares <ofjolivares@gmail.com>
2020-06-12 12:39:00 -0400

    Add twoFer

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const twoFer = (X = "two") => {
+export const twoFer = (X = "you") => {
   return `One for ${X}, one for me.`
 };
